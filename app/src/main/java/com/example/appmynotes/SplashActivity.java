45b02baf2025e243b7c7f2289aa7d800fe871d3d
Zoratoo <gui_zorato@hotmail.com>
2024-09-17 16:44:30 -0300

    Adição da splash

---
+++ app/src/main/java/com/example/appmynotes/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.example.appmynotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Handler handler;
+    private Runnable runnable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        imageView=findViewById(R.id.imageView);
+        imageView.setOnClickListener(e->{entrar();});
+        handler=new Handler();
+        runnable=() -> entrar();
+        handler.postDelayed(runnable,3000);
+        hideSystemUI();
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    private void entrar() {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+        handler.removeCallbacks(runnable);
+    }
+}
